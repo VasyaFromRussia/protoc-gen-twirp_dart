d5417bd53aa83fe9c13f289587a17b8bb2036f6c
Renat Sarymsakov <ragnor.rs@gmail.com>
2020-04-10 18:21:07 +0300

    fixed import

--- main.go
+++ main.go
@@ -6,9 +6,8 @@ import (
 	"os"
 	"strings"
 
-	"./generator"
-
 	plugin_go "github.com/gogo/protobuf/protoc-gen-gogo/plugin"
+	"github.com/ragnor-rs/protoc-gen-twirp_dart/generator"
 
 	gogogen "github.com/gogo/protobuf/protoc-gen-gogo/generator"
 	"github.com/golang/protobuf/proto"
